ef7e167427c0b0583e7b39edfb922d904360daf8
m <m.gregorio@aluno.ufabc.edu.br>
2018-07-13 00:07:57 -0300

    Projeto iniciado com spring initializr e comando de criação do banco no readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # prog-web
+
+create database "dinheiros" with owner=postgres encoding='utf8' connection limit=-1;
---
+++ dinheiros/src/main/java/br/com/progweb/dinheiros/DinheirosApplication.java
@@ -0,0 +1,12 @@
+package br.com.progweb.dinheiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinheirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinheirosApplication.class, args);
+	}
+}
